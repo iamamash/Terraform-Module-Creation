bb6b38ec4630a59cd5e187fa08ff2e971a55ccc2
Amash Ansari <ansari2002ksp@gmail.com>
2024-02-29 00:10:45 +0530

    Added configurations for Ansible

--- modules/instance/main.tf
+++ modules/instance/main.tf
@@ -79,9 +79,49 @@ resource "aws_instance" "private_instance" {
   subnet_id       = module.VPC.private_subnet_id[count.index] # Reference to the VPC module
   key_name        = module.SSH_KEYPAIR.aws_key_pairs          # Reference to the SSH Keypair module
   security_groups = [aws_security_group.private_sg.id]        # Reference to the security group for private instances
+
+  # Provisioner to install Ansible on the private instances
+  provisioner "remote-exec" {
+    # Connection details for the private instance
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = aws_instance.private_instance[count.index].public_ip
+    }
+
+    # Commands to install Ansible on the private instance
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install software-properties-common -y",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible -y",
+      "sudo apt install ansible -y"
+    ]
+  }
+
+  # Passing Elastic IP as an argument to the Ansible playbook
+  provisioner "local-exec" {
+    command = "ansible-playbook -e 'elastic_ip_1=${aws_eip.eip[0].public_ip},elastic_ip_2=${aws_eip.eip[1].public_ip}' ../../ansible/playbook.yml" # Command to execute the Ansible playbook
+
+    working_dir = "../../ansible/" # Working directory for the Ansible playbook                         
+  }
+
   tags = {
     Name : "${element(var.private_instance_name, count.index)}" # Name of the instance
   }
+}
+
+# Create an Elastic IP
+resource "aws_eip" "eip" {
+  count    = 2                                             # Create two Elastic IPs
+  instance = aws_instance.private_instance[count.index].id # Reference to the private instance
+}
+
+# Associate the Elastic IP with the EC2 instance
+resource "aws_eip_association" "eip_association" {
+  count         = 2                                             # Associate two Elastic IPs
+  instance_id   = aws_instance.private_instance[count.index].id # Reference to the private instance
+  allocation_id = aws_eip.eip[count.index].id                   # Reference to the Elastic IP
 }
 
 
