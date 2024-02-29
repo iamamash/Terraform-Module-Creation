1b8c6ba99154442e1fbde0a6bc8fb2dc86e1d8fe
Amash Ansari <42666741+iamamash@users.noreply.github.com>
2024-02-29 12:28:05 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -29,7 +29,7 @@ resource "aws_subnet" "private_subnet" {
   count             = 2 # Creates two private subnets
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.private_subnet_cidr[count.index]         # CIDR block for the private subnet
-  availability_zone = var.availability_zones[count.index]         # Availability zone for the private subnet
+  availability_zone = element(var.availability_zones, count.index) # Availability zone for the private subnet
 
   tags = {
     Name = "${var.vpc_name}-private-subnet-${count.index + 1}" # Name of the private subnet
